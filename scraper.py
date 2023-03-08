57504421cbcc2f2ae1966593c2d43738fd3c4c75
Alex <alexfox262@gmail.com>
2023-03-08 11:00:05 +0300

    Completed stage 2

--- scraper.py
+++ scraper.py
@@ -1,13 +1,25 @@
 import requests
 
+import re
 
-def request_get_quotes(url):
-    response = requests.get(url)
-    if response.status_code == 200 and 'content' in response.json():
-        print(response.json()['content'])
-    else:
-        print("Invalid quote resource!")
+from bs4 import BeautifulSoup
 
 
-user_url = str(input())
-request_get_quotes(user_url)
+def requests_get_summary(url):
+    response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    soup = BeautifulSoup(response.content, features='html.parser')
+    title = soup.find('title').text
+    summary = soup.find('meta', {'name': 'description'})['content']
+    result = {'title': title, 'description': summary}
+    return result
+
+
+def page_valid(url):
+    return re.search('articles|nature.com', url)
+
+
+user_url = str(input('Input the URL:\n'))
+if page_valid(user_url):
+    print(requests_get_summary(user_url))
+else:
+    print('Invalid page!')
