2413531221cccec2801e2fd0bb14a1687e58c0b4
Alex <alexfox262@gmail.com>
2023-03-12 15:03:55 +0300

    Completed stage 5. Project finished

--- scraper.py
+++ scraper.py
@@ -4,6 +4,12 @@ from bs4 import BeautifulSoup
 
 import string
 
+import os
+
+# import time
+
+# from datetime import datetime
+
 
 def get_content(url):
     r = requests.get(url)
@@ -19,21 +25,33 @@ def format_title(title):
     title = title.replace(' ', '_')
     return title
 
+"""
+def get_current_time():
+    timestamp = time.time()
+    date_time = datetime.fromtimestamp(timestamp)
+    return date_time.strftime('%d-%m-%Y, %H-%M-%S')
+"""
 
-user_url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
-response = requests.get(user_url)
-soap = BeautifulSoup(response.content, 'html.parser')
-all_articles = soap.findAll('li', class_='app-article-list-row__item')
-correct_articles = {}
+user_url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page='
+user_pages_qty = int(input())
+user_article_type = input()
+# time_stamp = get_current_time()
+for page_num in range(1, user_pages_qty + 1):
+    response = requests.get(user_url + str(page_num))
+    soap = BeautifulSoup(response.content, 'html.parser')
+    all_articles = soap.findAll('li', class_='app-article-list-row__item')
+    correct_articles = {}
 
-for _ in all_articles:
-    article_type = _.find('span', class_='c-meta__type').text
-    if article_type == 'News':
-        article_title = _.find('a', {'data-track-action': 'view article'})
-        inner_link = 'https://www.nature.com' + article_title['href']
-        content = get_content(inner_link)
-        correct_articles.update({format_title(article_title.text): content})
+    for _ in all_articles:
+        article_type = _.find('span', class_='c-meta__type').text
+        if article_type == user_article_type:
+            article_title = _.find('a', {'data-track-action': 'view article'})
+            inner_link = 'https://www.nature.com' + article_title['href']
+            content = get_content(inner_link)
+            correct_articles.update({format_title(article_title.text): content})
 
-for key, value in correct_articles.items():
-    with open(f'{key}.txt', 'wb') as file:
-        file.write(bytes(value, 'utf-8'))
+    os.makedirs(f'Page_{page_num}')
+    for key, value in correct_articles.items():
+        with open(f'Page_{page_num}//{key}.txt', 'wb') as file:
+            file.write(bytes(value, 'utf-8'))
+print('Saved all articles.')
